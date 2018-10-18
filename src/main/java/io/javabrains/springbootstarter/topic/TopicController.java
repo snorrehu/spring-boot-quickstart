2d0d9b806bd0c2cf4a2a9d7cacd980de85e0c8ad
snorrehu <snorre.hukkelaas@gmail.com>
2018-10-18 11:36:27 +0200

    Finished module without db

--- src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -1,8 +1,7 @@
 package io.javabrains.springbootstarter.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,16 +21,36 @@ public class TopicController {
     public List<Topic> getAllTopics(){
 
         //create topic objects "on the fly":
-        return topicService.getAlltTopics();
+        return topicService.getAllTopics();
+    }
+
+    //Map POST request (GET is default!), must specify!
+    //"Map this method on any POST request on URI /topics"
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    //Tell spring MVC that your request payload is going to contain a json representation of
+    //this topic instance. Take that request body and convert it to a topic instance and pass it to the
+    // addTopic method:
+    public void createTopic(@RequestBody Topic topic){
+        //Get post body, convert it to topic instance and add it to the list of topics:
+        topicService.addTopic(topic);
     }
 
     //The curly braces tells spring that "id" is a variable:
     @RequestMapping("/topics/{id}")
-    public Topic getTopic(String id){
+    //@Pathvariable tells spring that the variable comes from the path, as denoted above:
+    public Topic readTopic(@PathVariable String id){
         return topicService.getTopic(id);
     }
 
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id){
+        topicService.updateTopic(id,topic);
+    }
 
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id){
+        topicService.deleteTopic(id);
+    }
 
 
 }
--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -2,6 +2,7 @@ package io.javabrains.springbootstarter.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,17 +10,36 @@ import java.util.List;
 @Service
 public class TopicService {
 
-    private List<Topic> topics = Arrays.asList(
+    //Arraylist<> makes the Arrays.asList mutable!
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
                 new Topic("spring","Spring Framework","Spring Framework Description"),
                 new Topic("java","Java Framework","Java Framework Description"),
                 new Topic("javascript","Javascript Framework","Javascript Framework Description")
-    );
+    ));
 
-    public List<Topic> getAlltTopics(){
+    public List<Topic> getAllTopics(){
         return topics;
     }
 
     public Topic getTopic(String id){
         return topics.stream().filter(t -> t .getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for (int i = 0; i < topics.size(); i ++){
+            Topic t = topics.get(i);
+            if(t.getId().equals(id)){
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
     }
 }
